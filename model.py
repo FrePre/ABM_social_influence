462cfb94fd58c2f787dc1cb42850e090944cdc7b
FrePre <141215956+FrePre@users.noreply.github.com>
2023-08-06 12:37:48 +0200

    model.py

--- model.py
+++ model.py
@@ -1,14 +1,17 @@
+# Import der benötigten Bibliotheken
 import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
 import math
-xrespb = 1/2 # Relativer Einfluss auf Verantwortungsbewusstsein (A-B-Interaktion) und relativer Einfluss auf Problembewusstsein A-A-Interaktion 
+
+#Import von einigen Parametern für das Modell
+xrespb = 1/2 # Relativer Einfluss auf Verantwortungsbewusstsein (A-B-Interaktion) und relativer Einfluss auf Problembewusstsein (A-A-Interaktion) 
 maxeinflussB = 1 # Maximaler Einfluss von B-Agent auf A-Agent, d.h. auf dessen Problembe-wusstsein.
 maxeinflussA = 1/2 # Maximaler Einfluss von A-Agent auf A-Agent, d.h. auf dessen Verantwor-tungsbewusstsein.
 negativeinfluss = 0.01 # Negativer Einfluss auf das Problembewusstsein bei keiner Interaktion
-bounded = 2.5 # eingeschränkter Vertrauensradius
+bounded = 2.5 # Eingeschränkter Vertrauensradius
 
-# A-Agent
+# Definition der Klasse "Agent" für A-Agenten
 class Agent:
     def __init__(self, i, awareness, responsibility, bereitschaft, openness):
         self.awareness = awareness
@@ -19,21 +22,20 @@ class Agent:
         self.lastupdate = 0
     
         
-   # A trifft auf B
+   # Aktualisierung des Verantwortungs- und Problembewusstseins, wenn A-Agent auf A- oder B-Agenten trifft
 
     def update(self, other):
         self.lastupdate = 0
         op = (self.openness)/10
+
+        # A-Agent trifft auf B-Agent
         if isinstance(other, AgentB) == True:
             self.awareness = self.awareness + op * maxeinflussB 
             self.awareness = max(0,min(self.awareness,10))
             self.responsibility = self.responsibility + (op* maxeinflussB * xrespb) 
             self.responsibility = max(0,min(self.responsibility,10))
         
-        
-                
-        # A trifft auf A 
-        
+        # A-Agent trifft auf A-Agenten 
         elif isinstance(other, Agent) == True:
             if (other.bereitschaft - self.bereitschaft) >= -bounded and (other.bereitschaft - self.bereitschaft) < 0:
                 self.responsibility = self.responsibility - op* maxeinflussA
@@ -47,7 +49,7 @@ class Agent:
         self.bereitschaft = (self.awareness * self.responsibility)**0.5
         self.bereitschaft = max(0,min(self.bereitschaft,10))
 
-    # keine Interaktion
+    # Aktualisierung des Problembewusstsein, wenn A-Agent nicht interagiert
 
     def update_non_interaction(self):
          self.lastupdate = self.lastupdate + 1
@@ -57,17 +59,16 @@ class Agent:
             self.bereitschaft = (self.awareness * self.responsibility)**0.5
             self.bereitschaft = max(0,min(self.bereitschaft,10))
              
-# B-Agent
+# Definition der Klasse "AgentB" für B-Agenten
 class AgentB:
      def __init__(self, i):
             self.i = i
-# Netzwerk
+# Definition der Klasse für das Modell mit Netzwerk
 class Model_with_network:
     def __init__(self, n_agents, node_degree, rewiring_prob, notopen_Anzahl, anzahl_ABlink, n_agentsb, frequence, seed): 
         np.random.seed(seed)
         
         # Parameter zuweisen 
-        
         self.n_agents = n_agents #Anzahl A-Agenten
         self.node_degree = node_degree # durchschnittliche Anzahl der Links pro A-Agent
         self.rewiring_prob = rewiring_prob # Umverdrahtungswahrscheinlichkeit
@@ -76,8 +77,7 @@ class Model_with_network:
         self.n_agentsb = n_agentsb # Anzahl B-Agenten
         self.frequence = frequence # Interaktionswahrscheinlichkeit
 
-        # Agenten initialisieren
-        
+        # A-Agenten initialisieren
         listAgents = []
         for i in range(self.n_agents):
             if i <= self.notopen_Anzahl:
@@ -94,15 +94,13 @@ class Model_with_network:
         self.network = nx.watts_strogatz_graph(self.n_agents, self.node_degree, self.rewiring_prob, seed)
         self.network.edges
         
-        # die Agenten B haben keine Eigenschaften, sondern dienen nur als Sender
-        
+        # B-Agenten initialisieren (haben keine Eigenschaften, sondern dienen nur als Sender)
         self.agentsb = [AgentB (i = i + self.n_agents) for i in range(self.n_agentsb)]
         idsb = [ag.i for ag in self.agentsb]
         self.agentsbdict = dict (zip(idsb,self.agentsb))
       
         
-        # Graph für A-Agenten und B-Agenten
-        
+        # Graph für A-Agenten und B-Agenten erstellen
         self.g = nx.Graph()
         self.g.add_nodes_from(range(0, len(self.agents) + len(self.agentsb)))   
         for i in range (self.anzahl_ABlink) :
@@ -110,10 +108,12 @@ class Model_with_network:
             v = np.random.choice(self.agentsb)
             self.g.add_edge(u.i, v.i)
         self.g.add_edges_from (self.network.edges, weight = 0.3) 
-            
+
+        # alle Agenten in einem Dictionary speichern
         self.agentsalldict = self.agentsdict.copy()
-        self.agentsalldict.update(self.agentsbdict)        
-            
+        self.agentsalldict.update(self.agentsbdict)  
+        
+   # Aktualisierung eines  Zeitschrittes       
     def update_step(self):
         ids = [ag.i for ag in self.agents]
         np.random.shuffle(ids)  # mischt die Einträge in der id-Liste (in-place)
@@ -125,7 +125,7 @@ class Model_with_network:
                 potentialnachbar = list(self.g.neighbors(ag.i))
                 if not potentialnachbar == []: 
                     idsspeaker = np.random.choice(potentialnachbar)
-                    speaker = self.agentsalldict[idsspeaker]    # Sprecher zufällig ausgewählter NACHBAR 
+                    speaker = self.agentsalldict[idsspeaker]    # Sprecher zufällig ausgewählter Nachbar
 
                     ag.update(other= speaker)
             else:
